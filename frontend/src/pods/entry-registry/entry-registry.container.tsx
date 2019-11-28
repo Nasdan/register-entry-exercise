e9d952ac197c481566dd7658aaa593d042111550
Puncernau <gomezj16@emea.roche.com>
2019-11-28 14:57:48 +0100

    Update

--- frontend/src/pods/entry-registry/entry-registry.container.tsx
+++ frontend/src/pods/entry-registry/entry-registry.container.tsx
@@ -2,24 +2,18 @@ import * as React from 'react';
 import { useHistory } from 'react-router-dom';
 import { EntryRegistryComponent } from './entry-registry.component';
 import {RouterComponent} from 'core/router';
-import { LoginEntityVm, createEmptyLogin } from './entry-registry.vm';
-import { validateCredentials } from './entry-registry.api';
+import { EntryRegistryVm, createEmptyEntryRegistry } from './entry-registry.vm';
+import { saveEntryRegistry } from './entry-registry.api';
 
-export const EntryRegistryContainer = () => {
+export const EntryRegistryContainer = () => 
+{
   const history = useHistory();
-  const [initialLogin] = React.useState<LoginEntityVm>(createEmptyLogin());
+  const [initialEntryRegistry] = React.useState<EntryRegistryVm>(createEmptyEntryRegistry());
 
-  const handleLogin = (loginInfo: LoginEntityVm) => {
-    validateCredentials(loginInfo.login, loginInfo.password).then(
-      areValidCredentials => {
-        areValidCredentials
-          ? history.push(linkRoutes.hotelCollection)
-          : alert(
-              'invalid credentials, use admin/test, excercise: display a mui snackbar instead of this alert.'
-            );
-      }
-    );
+  const handleSubmit = (entryRegistryInfo: EntryRegistryVm) => 
+  {
+    saveEntryRegistry(entryRegistryInfo).then(history.push(linkRoutes.hotelCollection));
   };
 
-  return <EntryRegistryComponent onLogin={handleLogin} initialLogin={initialLogin} />;
+  return <EntryRegistryComponent onSubmit={handleSubmit} initialLogin={initialEntryRegistry} />;
 };
